c8c8247f54855cc6af654187657c7c5b486b0aa3
Jatinder011 <77622853+Jatinder011@users.noreply.github.com>
2021-03-23 13:13:09 +0530

    Delete FIB.py

--- FIB.py
+++
@@ -1,8 +0,0 @@
-def fib(months, offsprings):
-    parent = 1
-    child = 1
-    for y in range(months - 1):
-        child, parent = parent, parent + (child * offsprings)
-    return child
-
-fib(5, 3)
